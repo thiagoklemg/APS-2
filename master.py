1a040ac1fd48285eeec02f5f30bb6b4d0614ef77
kellydena <49529455+kellydena@users.noreply.github.com>
2019-11-11 10:54:23 -0300

    kelly, label feito e bt confirmar

--- master.py
+++ master.py
@@ -1,6 +1,8 @@
 from tkinter import *
 import time
 
+
+
 def HomePage():
     #inicia a tela inicial dentro de uma função
     JanelaInicial = Tk()
@@ -14,9 +16,28 @@ def HomePage():
 
         def Voltar():
             return JanelaPapel.destroy()
+        
         BotaoVoltar = Button(JanelaPapel, text="Voltar", width=10, command=Voltar)
-        BotaoVoltar.place(x=300, y=10)
+        BotaoVoltar.place(x=300, y=650)
+        #label para o usuario identificar se é reciclavel ou não
+        LabelPapel = Label(JanelaPapel,  text= 'CONFIRME SE É RECICLAVEL: \n  Exemplos de reciclaveis : \n Caixa de papelão, jonais, revistas, \n folhas de caderno, caixas, envelopes, cartazes entre outros. \n Exemplos de não reciclaveis: \n Papel higienico, guardanapo, papel laminado, papel plastificado, \n papel carbono, fita crepe, fotografia e bituca de cigarro.')
+        LabelPapel.pack()
+        LabelPapel.place(x=300, y=200)
+       #Janela para a localização (ultima janela)
+        def Confirmar():
+            JanelaConfirmar = Tk()
+            JanelaConfirmar.title('Localização')
+            #botao para destruir a janela da confirmar (localização)         
+            def Voltar():
+                return JanelaConfirmar.destroy()
+            BotaoVoltar = Button(JanelaConfirmar, text="Voltar", width=10, command=Voltar)
+            BotaoVoltar.place(x=300, y=650)
+            JanelaConfirmar.geometry('700x770+0+0')
+            JanelaConfirmar.mainloop()
+        
         JanelaPapel.geometry("700x770+0+0")
+        BotaoConfirmar = Button (JanelaPapel, text = "É recliclavel!", width = 10, command = Confirmar)
+        BotaoConfirmar.place (x=300, y= 550)
         JanelaPapel.mainloop()
 
 #TELA INICIAL
@@ -34,7 +55,7 @@ def HomePage():
         def Voltar():
             return JanelaVidro.destroy()
         BotaoVoltar = Button(JanelaVidro, text="Voltar", width=10, command=Voltar)
-        BotaoVoltar.place(x=300, y=10)
+        BotaoVoltar.place(x=300, y=650)
         JanelaVidro.geometry("700x770+0+0")
         JanelaVidro.mainloop()
 
@@ -53,7 +74,7 @@ def HomePage():
         def Voltar():
             return JanelaPlastico.destroy()
         BotaoVoltar = Button(JanelaPlastico, text="Voltar", width=10, command=Voltar)
-        BotaoVoltar.place(x=300, y=10)
+        BotaoVoltar.place(x=300, y=650)
         JanelaPlastico.geometry("700x770+0+0")
         JanelaPlastico.mainloop()
 
@@ -71,7 +92,7 @@ def HomePage():
         def Voltar():
             return JanelaMetal.destroy()
         BotaoVoltar = Button(JanelaMetal, text="Voltar", width=10, command=Voltar)
-        BotaoVoltar.place(x=300, y=10)
+        BotaoVoltar.place(x=300, y=650)
         JanelaMetal.geometry("700x770+0+0")
         JanelaMetal.mainloop()
 
@@ -98,4 +119,3 @@ def HomePage():
 #PROGRAMA PRINCIPAL
 #---chama a funcão em que ta todo o programa---
 HomePage()
-
