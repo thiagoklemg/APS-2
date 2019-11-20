a3ab2547386664fad9e6200fbef640e27ddc56d3
Thiago Klem <thiagoklemg@gmail.com>
2019-11-20 11:56:33 -0300

    update botao organico thiago

--- interface.py
+++ interface.py
@@ -614,6 +614,156 @@ def HomePage():
     BotaoMetal = Button(JanelaInicial, text='Metal', bg='yellow', width=15, height=1, font='Calibri, 14', command=Metal)
     BotaoMetal.place(relx=0.5, rely=0.6, anchor=CENTER)
 
+# Funcao para a janela do ORGANICO
+    # %%%%%%%%%%%%%%%%%%% JANELA ORGANICO %%%%%%%%% (INICIO)
+    def Organico():
+        JanelaOrganico = Tk()
+        JanelaOrganico.title("Organico")
+        JanelaOrganico.configure(background='brown')
+
+        # função para o botão voltar funcionar
+
+        def Voltar():
+            return JanelaOrganico.destroy()
+
+        BotaoVoltar = Button(JanelaOrganico, text="Voltar", bg='Light grey', width=15, height=1, font='Calibri, 14',
+                             command=Voltar)
+        BotaoVoltar.place(relx=0.5, rely=0.8, anchor=CENTER)
+        # lABEL que fica na janela de cada categoria identificando se é reciclavel ou nao
+        LabelOrganico = Label(JanelaOrganico,
+                              text='CONFIRME SE É RECICLAVEL: \n \n  Exemplos de reciclaveis : \n tampas, potes de alimentos (margarina), frascos, \n utilidades domésticas, embalagens de refrigerante. \n Exemplos de não reciclaveis: \n cabos de panela, tomadas, isopor, adesivos, \n espuma, teclados de computador, acrílicos.')
+        LabelOrganico.pack()  # comando reservado para funcioonar o label
+        LabelOrganico.configure(background='brown', font='Calibri, 16')
+        LabelOrganico.place(relx=0.5, rely=0.2, anchor=CENTER)  # localização do label
+
+        # @@@@@@@@@@ JANELA CONFIRMAR (LOCALIZAÇÃO)@@@@@@@@@@@@@@@@@ (INICIO)
+
+        def Confirmar():  # função para a janela caso o lixo seja reciclavel
+            JanelaConfirmar = Tk()  # abertura da janela
+            JanelaConfirmar.title('Confirmar')
+            JanelaConfirmar.configure(background='brown')
+
+            # botao para destruir a janela da confirmar (localização)
+
+            def Voltar():
+                return JanelaConfirmar.destroy()
+
+            # botão voltar da janela confirmar(localizção)
+            BotaoVoltar = Button(JanelaConfirmar, text="Voltar", bg='Light grey', width=15, height=1,
+                                 font='Calibri, 14', command=Voltar)
+            BotaoVoltar.place(relx=0.5, rely=0.8, anchor=CENTER)
+            # janela da localização e ses adereços
+            JanelaConfirmar.geometry('700x770+0+0')
+
+            def LocalProximo():
+                if InputStreet.get() != "" and InputNumber.get() != "" and InputCity.get() != "":
+                    userAddress = localization.getUserAddress(InputStreet.get(), InputNumber.get(), InputCity.get())
+                    result = localization.search(userAddress, 5)
+
+                    JanelaLocalProximo = Tk()
+                    JanelaLocalProximo.title('Endereço')
+                    JanelaLocalProximo.configure(background='brown')
+                    JanelaLocalProximo.geometry('700x770+0+0')
+
+                    LabelResultName = Label(JanelaLocalProximo,
+                                            text=result[1])
+                    LabelResultName.pack()
+                    LabelResultName.place(relx=0.5, rely=0.1, anchor=CENTER)
+                    LabelResultName.configure(background='brown', font='Calibri 18 bold')
+
+                    auxAddress = "Endereço: " + result[0]
+                    LabelAddress = Label(JanelaLocalProximo,
+                                         text=auxAddress)
+                    LabelAddress.pack()
+                    LabelAddress.place(relx=0.5, rely=0.2, anchor=CENTER)
+                    LabelAddress.configure(background='brown', font='Calibri 18')
+
+                    def Voltar():
+                        return JanelaLocalProximo.destroy()
+
+                    def VerRota():
+                        webbrowser.open_new(result[2])
+
+                    BotaoIr = Button(JanelaLocalProximo, text="Ver rota!", bg='Light grey', width=15, height=1,
+                                     font='Calibri, 14', command=VerRota)
+                    BotaoIr.place(relx=0.5, rely=0.6, anchor=CENTER)
+
+                    BotaoVoltar = Button(JanelaLocalProximo, text="Voltar", bg='Light grey', width=15, height=1,
+                                         font='Calibri, 14', command=Voltar)
+                    BotaoVoltar.place(relx=0.5, rely=0.7, anchor=CENTER)
+                    JanelaLocalProximo.geometry('700x770+0+0')
+                    JanelaLocalProximo.mainloop()
+
+            LabelDescription = Label(JanelaConfirmar,
+                                     text="Quer descobrir um ponto de coleta \nmais perto de você?\n Preencha o formulário (Todos os campos são obrigatórios)")
+            LabelDescription.pack()
+            LabelDescription.place(relx=0.5, rely=0.1, anchor=CENTER)
+            LabelDescription.configure(background='brown', font='Calibri 18')
+
+            LabelStreet = Label(JanelaConfirmar, text="Rua:")
+            LabelStreet.pack()
+            LabelStreet.place(relx=0.5, rely=0.2, anchor=CENTER)
+            LabelStreet.configure(background='brown', font='Calibri 18 bold')
+
+            LabelStreet2 = Label(JanelaConfirmar, text="(Exemplo: Avenida Norte e Sul)")
+            LabelStreet2.pack()
+            LabelStreet2.place(relx=0.5, rely=0.25, anchor=CENTER)
+            LabelStreet2.configure(background='brown', font='Calibri 12')
+
+            InputStreet = Entry(JanelaConfirmar, font=('Ubuntu', 15))
+            InputStreet.place(relx=0.5, rely=0.3, anchor=CENTER)
+
+            LabelNumber = Label(JanelaConfirmar, text="Número:")
+            LabelNumber.pack()
+            LabelNumber.place(relx=0.5, rely=0.35, anchor=CENTER)
+            LabelNumber.configure(background='brown', font='Calibri 18 bold')
+
+            LabelNumber2 = Label(JanelaConfirmar, text="(Exemplo: 194)")
+            LabelNumber2.pack()
+            LabelNumber2.place(relx=0.5, rely=0.4, anchor=CENTER)
+            LabelNumber2.configure(background='brown', font='Calibri 12')
+
+            InputNumber = Entry(JanelaConfirmar, font=('Ubuntu', 15))
+            InputNumber.place(relx=0.5, rely=0.45, anchor=CENTER)
+
+            LabelCity = Label(JanelaConfirmar, text="Cidade:")
+            LabelCity.pack()
+            LabelCity.place(relx=0.5, rely=0.5, anchor=CENTER)
+            LabelCity.configure(background='brown', font='Calibri 18 bold')
+
+            LabelCity2 = Label(JanelaConfirmar, text="(Exemplo: Campinas)")
+            LabelCity2.pack()
+            LabelCity2.place(relx=0.5, rely=0.55, anchor=CENTER)
+            LabelCity2.configure(background='brown', font='Calibri 12')
+
+            InputCity = Entry(JanelaConfirmar, font=('Ubuntu', 15))
+            InputCity.place(relx=0.5, rely=0.6, anchor=CENTER)
+
+            BotaoLocalizacao = Button(JanelaConfirmar, text="Pesquisar", bg='Light grey', width=15, height=1,
+                                      font='Calibri, 14', command=LocalProximo)
+            BotaoLocalizacao.place(relx=0.5, rely=0.7, anchor=CENTER)
+            JanelaConfirmar.geometry("700x770+0+0")
+            JanelaConfirmar.mainloop()
+
+        # @@@@@@@@@@@@@@@@ JANELA CONFIRMAR (LOCALIZAÇÃO) @@@@@@@@@@@ (FINAL)
+
+        # botao caso o lixo seja reciclavel e diecionamento para a janela confirmar (Localizção)
+        BotaoConfirmar = Button(JanelaOrganico, text="É recliclavel!", bg='Light grey', width=15, height=1,
+                                font='Calibri, 14', command=Confirmar)
+        BotaoConfirmar.place(relx=0.5, rely=0.5, anchor=CENTER)
+        JanelaOrganico.geometry("700x770+0+0")
+        JanelaOrganico.mainloop()
+
+    # %%%%%%%%%%%%%%%%%%%% JANELA Organico %%%%%%%%%%%% (FINAL)
+
+    # *******************TELA INICIAL*******************(BOTAO Organico INICO)
+    # Chamando a função Organico que esta acima e tem todas as funcionalidades necessárias
+    BotaoOrganico = Button(JanelaInicial, text='Organico', bg='brown', width=15, height=1, font='Calibri, 14',
+                           command=Organico)
+    BotaoOrganico.place(relx=0.5, rely=0.7, anchor=CENTER)
+
+    # ******************* TELA INICIAL **************** (BOTAO Organico FINAL)
+
     # *******************
 
     # Função para Destruir a TELA INICIAL
